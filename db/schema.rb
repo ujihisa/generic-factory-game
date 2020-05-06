3be2ce91a1630d801e5d558535bf8b3cf90b3295
ujihisa <ujihisa@users.noreply.github.com>
2020-05-05 17:08:52 -0700

    rails g scaffold player name:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_06_000826) do
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
