956c8f47f230dc0410305abb980e50c9bf1af40b
ujihisa <ujihisa@users.noreply.github.com>
2020-05-07 16:08:20 -0700

    Better looking

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,11 +1,12 @@
 class Player < ApplicationRecord
   validates_uniqueness_of :name
   validates :name, length: { in: 1..255 }
+  has_many :games
 
   def best_game
     Game.
       where('version = ? AND player_id = ? AND 1000 <= money', GenericFactoryGame::VERSION, id).
-      order('month ASC').
+      order(month: :asc).
       limit(1).
       first
   end
@@ -13,7 +14,7 @@ class Player < ApplicationRecord
   def previous_best_game
     Game.
       where('version = ? AND player_id = ? AND 1000 <= money', GenericFactoryGame::PREVIOUS_VERSION, id).
-      order('month ASC').
+      order(month: :asc).
       limit(1).
       first
   end
