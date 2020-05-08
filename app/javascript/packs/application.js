e635966ced71d3567f554d857b7b204e473bc601
ujihisa <ujihisa@users.noreply.github.com>
2020-05-07 23:02:58 -0700

    Prefer yarn to bundle for bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,6 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import '../stylesheets/application'
+import './bootstrap_custom.js'
