71346bb7080c79e8cce1134300c606cb7085ba22
ujihisa <ujihisa@users.noreply.github.com>
2020-05-09 19:13:57 -0700

    SubscribeIngredient also requires credit

--- app/javascript/components/ActionMenu.js
+++ app/javascript/components/ActionMenu.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Bank from "./Bank"
+import SubscribeIngredient from "./SubscribeIngredient"
 import GFG from '../gfg'
 
 class ActionMenu extends React.Component {
@@ -10,6 +11,8 @@ class ActionMenu extends React.Component {
       debt: props.debt,
       credit: props.credit,
       cash: props.cash,
+      ingredientSubscription: props.ingredientSubscription,
+      subscribe_ingredients_game_url: props.subscribe_ingredients_game_url,
     };
   }
 
@@ -18,8 +21,6 @@ class ActionMenu extends React.Component {
       return null
     return (
       <React.Fragment>
-        <h2>Actions</h2>
-
         {
           (0 < this.state.credit || 0 < this.state.debt)
             ? <button type="button" className="btn btn-secondary" data-toggle="modal" data-target="#exampleModal">
@@ -50,6 +51,55 @@ class ActionMenu extends React.Component {
         </div>
 
         <br/><br/>
+
+        {
+          (10 <= this.state.credit || 0 < this.state.ingredientSubscription)
+            ?  <button type="button" className="btn btn-secondary" data-toggle="modal" data-target="#subscribeIngredientModal">
+              📦 Subscribe Ingredient
+            </button>
+            :  <button type="button" className="btn btn-secondary" title="You need at least 10 credit" disabled>
+              📦 Subscribe Ingredient
+            </button>
+        }
+
+        <div className="modal" id="subscribeIngredientModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <form action={this.props.subscribe_ingredients_game_url} acceptCharset="UTF-8" data-remote="true" method="post">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalLabel">📦 Subscribe Ingredient</h5>
+                  <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <SubscribeIngredient cash={this.state.cash} ingredientSubscription={this.state.ingredientSubscription} />
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <br/><br/>
+
+        <form action={this.props.end_month_game_url} acceptCharset="UTF-8" data-remote="true" method="post">
+          <small>
+            Estimated new cash: <b>{ GFG.numberToCurrency(this.props.estimate_cash) }</b>
+          </small>
+          <br/>
+          {
+            (this.props.estimate_status == 'game_over') &&
+              <div className="alert alert-danger" role="alert">
+                Your cash will run out! Try borrowing some cash to survive.
+              </div>
+          }
+          <input type="submit" name="commit" value="End month"  data-disable-with="End month"
+            className={`btn btn-lg ${
+            {in_progress: 'btn-primary', game_over: 'btn-danger', completed: 'btn-success'}[this.props.estimate_status]
+          }`} />
+          <small className="form-text text-muted">
+            You employees work to produce product, and you deliver the accumulated product to your customers until they're satisfied.
+          </small>
+        </form>
+
       </React.Fragment>
     );
   }
@@ -60,7 +110,11 @@ ActionMenu.propTypes = {
   cash: PropTypes.number,
   debt: PropTypes.number,
   credit: PropTypes.number,
+  ingredientSubscription: PropTypes.number,
   action: PropTypes.string,
   formAuthenticityToken: PropTypes.string,
+  subscribe_ingredients_game_url: PropTypes.string,
+  end_month_game_url: PropTypes.string,
+  estimate_cash: PropTypes.number,
 };
 export default ActionMenu
