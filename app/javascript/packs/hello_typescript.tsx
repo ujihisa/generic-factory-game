c7ceb93552996ff361c30ecabfa2195609979b07
Kazuma Ebina <kazuma.ebina@quipper.com>
2020-06-03 07:05:52 +0000

    Hello world from TSX
    bundle exec rails webpacker:install:typescript
    cf. https://github.com/rails/webpacker/blob/v5.1.1/docs/typescript.md

---
+++ app/javascript/packs/hello_typescript.tsx
@@ -0,0 +1,5 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_typescript' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb.
+
+const message: string = 'Hello world from TSX'
+console.log(message);
