75abb33a4f0f412f090c979151f713e3f2537148
Kazuma Ebina <kazuma.ebina@quipper.com>
2020-06-03 07:16:33 +0000

    Write TSX

--- app/javascript/packs/hello_typescript.tsx
+++ app/javascript/packs/hello_typescript.tsx
@@ -1,5 +1,9 @@
 // Run this example by adding <%= javascript_pack_tag 'hello_typescript' %> to the head of your layout file,
 // like app/views/layouts/application.html.erb.
+import React from 'react'
 
-const message: string = 'Hello world from TSX'
-console.log(message);
+function Hello({ message }: { message: string }) {
+  return <div>{message}</div>
+}
+
+console.log(<Hello message="Hello world from TSX" />);
