df64bd2e1dc90789fb919bd0a25170cb6674c48c
ujihisa <ujihisa@users.noreply.github.com>
2020-06-02 23:55:07 -0700

    For compatibility

--- db/migrate/20200531185208_add_darklaunch.rb
+++ db/migrate/20200531185208_add_darklaunch.rb
@@ -7,7 +7,11 @@ class AddDarklaunch < ActiveRecord::Migration[6.0]
 
     reversible do |dir|
       dir.up do
-        Darklaunch.create!(key: 'enable-bank-client-side-adjuetment-buttons', player: Player.find(1))
+        if player = Player.find(1)
+          Darklaunch.create!(
+            key: 'enable-bank-client-side-adjuetment-buttons',
+            player: player)
+        end
       end
     end
   end
