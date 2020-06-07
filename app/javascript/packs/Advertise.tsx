9af986c93d00e5420b56c8cc19bf9b1db7d7d598
ujihisa <ujihisa@users.noreply.github.com>
2020-06-06 19:35:44 -0700

    Oops

--- app/javascript/packs/Advertise.tsx
+++ app/javascript/packs/Advertise.tsx
@@ -20,8 +20,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    // lng: "en",
-    lng: "ja",
+    lng: "en",
     fallbackLng: "en",
 
     keySeparator: false, // we do not use keys in form messages.welcome
