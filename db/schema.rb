47ffb9be33cef08a6516a1db0f3d91bac06b405b
ujihisa <ujihisa@users.noreply.github.com>
2021-04-03 15:53:18 -0700

    Upgrade ruby 2.7.2 -> 3.0.0
    * Heroku finally supports 3.0.0 now

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_06_222711) do
+ActiveRecord::Schema.define(version: 2020_06_07_173920) do
 
   create_table "admins", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 2020_06_06_222711) do
   create_table "darklaunches", force: :cascade do |t|
     t.string "key", null: false
     t.integer "player_id", null: false
-    t.datetime "created_at", precision: 6, default: "2020-11-22 05:51:20", null: false
-    t.datetime "updated_at", precision: 6, default: "2020-11-22 05:51:20", null: false
+    t.datetime "created_at", precision: 6, default: "2020-06-04 21:25:55", null: false
+    t.datetime "updated_at", precision: 6, default: "2020-06-04 21:25:55", null: false
     t.index ["key"], name: "index_darklaunches_on_key"
     t.index ["player_id"], name: "index_darklaunches_on_player_id", unique: true
   end
@@ -79,6 +79,14 @@ ActiveRecord::Schema.define(version: 2020_06_06_222711) do
 
   create_table "players", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "translations", force: :cascade do |t|
+    t.string "lang"
+    t.string "key"
+    t.string "value"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
