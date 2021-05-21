145fff0bd5d85f73d6fb45568ac178b7c285b785
ujihisa <ujihisa@users.noreply.github.com>
2021-05-21 10:03:23 -0700

    bundle lock --update

---
+++ update_commit_and_push.rb
@@ -0,0 +1,22 @@
+if `git branch --show-current`.chomp != 'master'
+  abort 'Run in master branch.'
+end
+
+system('git pull --rebase', exception: true)
+system('doo -b bundle update', exception: true)
+if `git diff -- Gemfile.lock` != ''
+  system("git add Gemfile.lock && git commit -m 'bundle lock --update'", exception: true)
+end
+
+system('doo yarn upgrade', exception: true)
+if `git diff -- yarn.lock` != ''
+  system("git add ./yarn.lock && git commit -m 'yarn upgrade'", exception: true)
+end
+
+system('doo -b bin/rails test', exception: true)
+
+system('git push', exception: true)
+
+puts 'All done'
+
+system('docker-compose down', exception: true)
