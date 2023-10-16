d92d745bd785dab5a2fadb9d4c4e4587b8706a51
ujihisa <ujihisa@users.noreply.github.com>
2023-10-16 16:16:41 -0700

    Move models from old to new

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,40 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+  }
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'games/highscore'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :games, except: [:edit, :update] do
+    member do
+      post 'create_storages'
+
+      post 'buy_ingredients'
+      post 'subscribe_ingredients'
+
+      post 'end_month'
+
+      post 'borrow_money'
+
+      post 'force_change' if Rails.env.development?
+
+      post 'hire'
+      post 'advertise'
+      post 'factory_assign'
+      post 'factory_buyinstall'
+
+      post 'contracts/cancel', to: 'contracts#cancel'
+      post 'contracts/create', to: 'contracts#create'
+    end
+
+    resources :contracts, only: [:create]
+  end
+
+  resources :players, only: [:new, :create]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'admin', to: 'admin#index'
+
+  root 'home#index'
 end
