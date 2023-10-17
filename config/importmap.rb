8bd1d2c6893046a3c1234210120040678207af05
ujihisa <ujihisa@users.noreply.github.com>
2023-10-17 14:33:18 -0700

    WIP bootstrap. JS should be ready but no CSS yet

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@4.3.1/dist/js/bootstrap.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
+pin "popper.js", to: "https://ga.jspm.io/npm:popper.js@1.16.1/dist/umd/popper.js"
